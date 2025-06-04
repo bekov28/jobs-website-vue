9e0cac4671f14b2ff12d3e06e6ce1c47c7a920e9
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-04 23:53:29 +0900

    navbar component created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,9 +7,12 @@ export default {
         sans: ["Poppins", "sans-serif"],
       },
       gridTemplateColumns: {
-        '70/30': '70% 28%'
-      }
+        "70/30": "70% 28%",
+      },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [],
 };
