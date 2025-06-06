24db5e12e5690362f9c89e63988317c52702b6d1
Bekov28 <orziqulov_berdiyor@mail.ru>
2025-06-06 18:23:11 +0900

    BackButton added and other fixes

--- src/components/JobListings.vue
+++ src/components/JobListings.vue
@@ -4,6 +4,7 @@ import JobListing from "@/components/JobListing.vue";
 import { ref, defineProps, onMounted, reactive } from "vue";
 import { RouterLink } from "vue-router";
 import axios from "axios";
+import PulseLoader from "vue-spinner/src/PulseLoader.vue";
 
 defineProps({
   limit: Number,
@@ -24,7 +25,7 @@ onMounted(async () => {
     const response = await axios.get("http://localhost:8000/jobs");
     state.jobs = response.data;
   } catch (error) {
-    console.error("Error fetching error", error);
+    console.error("Error fetching jobs", error);
   } finally {
     state.isLoading = false;
   }
@@ -35,7 +36,14 @@ onMounted(async () => {
   <section class="bg-blue-50 px-4 py-10">
     <div class="container-xl lg:container m-auto">
       <h2 class="text-3xl font-bold text-green-500 mb-6 text-center">Browse Jobs</h2>
-      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+
+      <!-- Show loading spinner while loading is true -->
+      <div v-if="state.isLoading" class="text-center text-gray-500 py-6">
+        <PulseLoader />
+      </div>
+
+      <!-- Show job listing when done loading -->
+      <div v-else class="grid grid-cols-1 md:grid-cols-3 gap-6">
         <JobListing
           v-for="job in state.jobs.slice(0, limit || state.jobs.length)"
           :key="job.id"
